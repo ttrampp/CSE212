61050e6093384e89ad045289550a34633f5d8a6c
ttrampp <ttrampp@msn.com>
2025-07-30 18:59:06 -0600

    Wk05 Code: Recursion all five problems

--- week05/code/Recursion.cs
+++ week05/code/Recursion.cs
@@ -1,4 +1,7 @@
 using System.Collections;
+using System.Diagnostics.CodeAnalysis;
+using System.Text.RegularExpressions;
+using NuGet.Frameworks;
 
 public static class Recursion
 {
@@ -15,7 +18,16 @@ public static class Recursion
     public static int SumSquaresRecursive(int n)
     {
         // TODO Start Problem 1
-        return 0;
+        //BASE CASE
+        //If the number is 0 or less, we stop here and return 0--avoiding going on forever
+        if (n <= 0)
+        {
+            return 0;
+        }
+
+        //SMALLER PROBLEM
+        //Square n, call function again using a smaller number, add them together
+        return (n * n) + SumSquaresRecursive(n - 1);
     }
 
     /// <summary>
@@ -40,6 +52,27 @@ public static class Recursion
     public static void PermutationsChoose(List<string> results, string letters, int size, string word = "")
     {
         // TODO Start Problem 2
+        //BASE CASE
+        //If the word we've built is the right length, we are done --- add it to the results list
+        if (word.Length == size)
+        {
+            //Add the word to the list of results--save the permutation
+            results.Add(word);
+        }
+        else
+        {
+            //SMALLER PROBLEM
+            //try adding each of the remaining letters-- One at a time
+            for (int i = 0; i < letters.Length; i++)
+            {
+                //remove the letter at index i so we don't reuse it in the same word; new string without the letter at i
+                string remaining = letters.Remove(i, 1);
+
+                //add that letter to the word we are building
+                // recursivly call the function again with the smaller list and the longer word
+                PermutationsChoose(results, remaining, size, word + letters[i]);
+            }
+        }
     }
 
     /// <summary>
@@ -86,6 +119,12 @@ public static class Recursion
     /// </summary>
     public static decimal CountWaysToClimb(int s, Dictionary<int, decimal>? remember = null)
     {
+        //create the dictionary if it is null
+        if (remember == null)
+        {
+            remember = new Dictionary<int, decimal>();
+        }
+
         // Base Cases
         if (s == 0)
             return 0;
@@ -97,10 +136,26 @@ public static class Recursion
             return 4;
 
         // TODO Start Problem 3
+        //^comment says to start problem here but i added a CREATE DICTIONARY above the base case
+        // if we have already solved this exact problem, use the save
+        if (remember.ContainsKey(s))
+            return remember[s];
+
+        //SMALLER PROBLEM
+        //Total ways to reach stair s = ways to get to (s-1) + (s-2) + (s-3)
+        decimal ways = CountWaysToClimb(s - 1, remember) + CountWaysToClimb(s - 2, remember) + CountWaysToClimb(s - 3, remember);
+
+        //Save the answer so we don't have to recalculate it next time
+        remember[s] = ways;
+
+        //return the total ways for this 's'
+        return ways;
+
 
         // Solve using recursion
-        decimal ways = CountWaysToClimb(s - 1) + CountWaysToClimb(s - 2) + CountWaysToClimb(s - 3);
-        return ways;
+        //decimal ways = CountWaysToClimb(s - 1) + CountWaysToClimb(s - 2) + CountWaysToClimb(s - 3);
+        //return ways;
+        
     }
 
     /// <summary>
@@ -119,6 +174,27 @@ public static class Recursion
     public static void WildcardBinary(string pattern, List<string> results)
     {
         // TODO Start Problem 4
+        //BASE CASE
+        //If the pattern has no * left, then it is complete
+        //meaning we have made all the replacements, so we just add it to the final
+        if (!pattern.Contains('*'))
+        {
+            results.Add(pattern);
+            return;
+        }
+
+        //RECURSIVE CASE
+        //Let us find the 1st * and replace it with both '0' and '1'. Each one creates a smaller problem to solve.
+        int index = pattern.IndexOf('*');   //Find the first wildcard (*)
+
+        //Smaller version 1 -- replace * with 0
+        string withZero = pattern.Substring(0, index) + '0' + pattern[(index + 1)..];
+        WildcardBinary(withZero, results);  //Recursively solve this smaller problem
+
+        //Smaller version 2 -- replace * with 1
+        string withOne = pattern.Substring(0, index) + '1' + pattern[(index + 1)..];
+        WildcardBinary(withOne, results);  //Recursively solve this one too
+
     }
 
     /// <summary>
@@ -132,11 +208,41 @@ public static class Recursion
         if (currPath == null) {
             currPath = new List<ValueTuple<int, int>>();
         }
-        
+
         // currPath.Add((1,2)); // Use this syntax to add to the current path
 
         // TODO Start Problem 5
         // ADD CODE HERE
+
+        //BASE CASE
+
+        //Make sure this move is valid--bounds, not a wall, not already visited
+        if (!maze.IsValidMove(currPath, x, y))
+            return;
+
+        //add current position to the path
+        currPath.Add((x, y));
+
+        //If we have reached the end square, save the path and backtrack
+        if (maze.IsEnd(x, y))
+        {
+            results.Add(currPath.AsString());   //save this completed path
+            currPath.RemoveAt(currPath.Count - 1);  //backtrack to explore other paths
+            return;
+        }
+
+        //RECURSIVE CASE
+        //SMALLER PROBLEMS
+        //Explore each direction one step at a time
+        SolveMaze(results, maze, x + 1, y, currPath); //try moving down
+        SolveMaze(results, maze, x - 1, y, currPath); //try moving up
+        SolveMaze(results, maze, x, y + 1, currPath); //try moving right
+        SolveMaze(results, maze, x, y - 1, currPath); //try moving left
+
+        //Bactracking
+        //Remove the last square before returning
+        currPath.RemoveAt(currPath.Count - 1);
+        //}
 
         // results.Add(currPath.AsString()); // Use this to add your path to the results array keeping track of complete maze solutions when you find the solution.
     }
